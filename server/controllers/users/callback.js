503070c6bb19bde0ae8ca110966302d3879b0831
LeeJongBum <qja965@gmail.com>
2023-07-03 09:05:56 +0900

    done

--- server/controllers/users/callback.js
+++ server/controllers/users/callback.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const axios = require('axios');
+require("dotenv").config();
+const axios = require("axios");
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 module.exports = async (req, res) => {
   // req의 body로 authorization code가 들어옵니다. console.log를 통해 서버의 터미널창에서 확인해보세요!
-  console.log(req.body);
+  console.log(req.body.authorizationCode);
   // authorization code를 이용해 access token을 발급받기 위한 post 요청을 보냅니다. 다음 링크를 참고하세요.
   // https://docs.github.com/en/free-pro-team@latest/developers/apps/identifying-and-authorizing-users-for-github-apps#2-users-are-redirected-back-to-your-site-by-github
   try {
     const result = await axios({
-      method: 'post',
+      method: "post",
       url: `https://github.com/login/oauth/access_token`,
       headers: {
-        accept: 'application/json',
+        accept: "application/json",
       },
       data: {
         client_id: CLIENT_ID,
@@ -21,9 +21,8 @@ module.exports = async (req, res) => {
       },
     });
     const accessToken = result.data.access_token;
-
     return res.status(200).send({ accessToken });
   } catch (err) {
-    return res.status(401).send({ message: 'error' });
+    return res.status(401).send({ message: "error" });
   }
 };
--- server/controllers/users/userInfo.js
+++ server/controllers/users/userInfo.js
@@ -1,14 +1,14 @@
-const axios = require('axios');
-const serverResource = require('../../data/data.js');
+const axios = require("axios");
+const serverResource = require("../../data/data.js");
 
 module.exports = async (req, res) => {
   const { accessToken } = req.body;
-
+  console.log(req.body);
   // 클라이언트에서 전달받은 access token를 이용해 사용자의 정보를 가져옵니다. 다음 링크를 참고하세요.
   // https://docs.github.com/en/rest/users/users#get-the-authenticated-user
   // 이때 요청하는 서버는 Github의 Authorization 서버가 아닌 Resource Server입니다.
   return axios
-    .get('https://api.github.com/user', {
+    .get("https://api.github.com/user", {
       headers: {
         Authorization: `token ${accessToken}`,
       },
